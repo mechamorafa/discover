cd91546e73fafe97f6a27cd1a04ebc8252a90059
J. Rafael Ramos <rafaelramos@live.com>
2023-06-12 07:23:58 -0300

    update on files

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+//getElementByID()
+const elementByID = document.getElementById('blog-title')
+console.log(elementByID)
+
+//getElementByClassName()
+const elementByClass = document.getElementsByClassName('one')
+console.log(elementByClass)
