a69cefd6f8a87cc64af03452c30e0c16fdc66cfc
J. Rafael Ramos <rafaelramos@live.com>
2023-06-12 07:28:19 -0300

    new classes

--- DOM/script.js
+++ DOM/script.js
@@ -5,3 +5,15 @@ console.log(elementByID)
 //getElementByClassName()
 const elementByClass = document.getElementsByClassName('one')
 console.log(elementByClass)
+
+//getElementByTagName()
+const elementByTag = document.getElementsByTagName('head')
+console.log(elementByTag)
+
+//querySelector()
+const element = document.querySelector('.one')
+console.log(element)
+
+//querySelectorAll()
+const elementAll = document.querySelectorAll('[src]')
+console.log(elementAll)
