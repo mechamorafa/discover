7a032700c0d9116cf19ee40eeb124189ca14c7e6
J. Rafael Ramos <rafaelramos@live.com>
2023-06-13 13:17:52 -0300

    require

--- node/require.js
+++ node/require.js
@@ -1,2 +1,5 @@
-const path = require('path')
-console.log(path.basename(__filename))
+//const path = require('path')
+//console.log(path.basename(__filename))
+
+const myModule = require('./exports.js')
+console.log(myModule)
