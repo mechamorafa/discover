d87b86e986eb6a183e65c8acd52fb30ffc3557e5
J. Rafael Ramos <rafaelramos@live.com>
2023-06-17 13:56:52 -0300

    Update on files
    Installed the express and ejs

---
+++ ejs/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res){
+    res.render("index")
+})
+
+app.get("/sobre", function (req, res){
+    res.render("about")
+})
